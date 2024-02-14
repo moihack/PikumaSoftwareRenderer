1c654163140ac62bda5506b50ffa2e1067018bc9
moihack <moihack@users.noreply.github.com>
2024-02-14 17:39:50 +0200

    Defining Header Files & Coding New Header Files
    Splitting main.c to other .h and .c files respectively.

---
+++ src/display.c
@@ -0,0 +1,135 @@
+#include "display.h"
+#include <stdio.h> // for stderr
+
+// definition of extern global variables
+// declared in display.h but also need to be defined somewhere (https://learn.microsoft.com/en-us/cpp/error-messages/tool-errors/linker-tools-error-lnk2001?view=msvc-170)
+// otherwise LNK2001 (and finally LNK1120) errors will occur
+// also check declarations in display.h for extern keyword use
+SDL_Window* window = NULL;
+SDL_Renderer* renderer = NULL;
+uint32_t* color_buffer = NULL; //pointer to first element in 1D array of 4 byte color values
+SDL_Texture* color_buffer_texture = NULL;
+int window_width = 800; // int just for code simplicity according to pikuma
+int window_height = 600;
+
+// NOTE from : https://en.wikipedia.org/wiki/Void_type
+// The C syntax to declare a (non-variadic) function 
+// with an as-yet-unspecified number of parameters, 
+// e.g. void f() above, was deprecated in C99.[3] 
+// In C23 (and C++), a function prototype with empty parentheses 
+// declares a function with zero parameters.[4][5]
+bool initialize_window(void)
+{
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+	{
+		fprintf(stderr, "Error initializing SDL.\n");
+		return false;
+	}
+
+	// Use SDL to query what is the max width,height of the display
+	SDL_DisplayMode display_mode;
+	SDL_GetCurrentDisplayMode(0, &display_mode);
+	window_width = display_mode.w;
+	window_height = display_mode.h;
+
+	// Create an SDL Window
+	window = SDL_CreateWindow(
+		NULL,
+		SDL_WINDOWPOS_CENTERED,
+		SDL_WINDOWPOS_CENTERED,
+		window_width,
+		window_height,
+		SDL_WINDOW_BORDERLESS
+	);
+
+	if (!window)
+	{
+		fprintf(stderr, "Error creating SDL window.\n");
+		return false;
+	}
+
+	// Create an SDL renderer
+	renderer = SDL_CreateRenderer(window, -1, 0);
+
+	if (!renderer)
+	{
+		fprintf(stderr, "Error creating SDL renderer.\n");
+		return false;
+	}
+
+	// This line enables fullscreen mode, otherwise it would be borderless fullscreen windowed instead
+	SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN);
+
+	return true;
+}
+
+void draw_grid(void) // basically a copy of clear_color_buffer with modified step size
+{
+	for (int y = 0; y < window_height; y += 10)
+	{
+		for (int x = 0; x < window_width; x += 10)
+		{
+			//if (x % 10 == 0 || y % 10 == 0) { // for "real grid" - remember to revert step size to y++ and x++
+			color_buffer[(window_width * y) + x] = 0xFF333333;
+			//}
+		}
+	}
+}
+
+void draw_rect(int x, int y, int width, int height, uint32_t color) // yet again a similar to the clear color buffer function
+{
+	// pikuma's solution
+	for (int i = 0; i < width; i++)
+	{
+		for (int j = 0; j < height; j++)
+		{
+			int current_x = x + i;
+			int current_y = y + j;
+			color_buffer[(window_width * current_y) + current_x] = color;
+		}
+	}
+
+	// my solution - practically the same but without local variables inside the for loop
+	// also my solution colors the buffer per row while pikuma's per column
+	/*for (int h = y; h < y + height; h++) // for each row in rect
+	{
+		for (int w = x; w < x+width; w++) // for each column per row in rect
+		{
+			color_buffer[ (window_width*h) + w ] = color; // color row of rect
+		}
+	} */
+}
+
+void render_color_buffer(void)
+{
+	SDL_UpdateTexture(
+		color_buffer_texture,
+		NULL,
+		color_buffer,
+		(int)(window_width * sizeof(uint32_t))
+	);
+	SDL_RenderCopy(renderer, color_buffer_texture, NULL, NULL);
+}
+
+void clear_color_buffer(uint32_t color)
+{
+	for (int y = 0; y < window_height; y++)
+	{
+		for (int x = 0; x < window_width; x++)
+		{
+			// since color_buffer is 1D array
+			// a single for loop could have been used instead
+			// for the time being code just follows along with the course  
+			color_buffer[(window_width * y) + x] = color;
+		}
+	}
+}
+
+void destroy_window(void)
+{
+	// destroy things in reverse order of creating them
+	free(color_buffer);
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+}
---
+++ src/display.h
@@ -0,0 +1,43 @@
+#ifndef DISPLAY_H
+#define DISPLAY_H
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <SDL.h>
+
+// only declaration
+// The extern keyword means "declare without defining". 
+// From : https://stackoverflow.com/a/1433387
+// Using extern is only of relevance when the program you're building consists
+// of multiple source files linked together, where some of the variables defined,
+// for example, in source file file1.c need to be referenced in other source files, such as file2.c
+//
+// Best way to declare and define global variables
+// The clean, reliable way to declare and define global variables 
+// is to use a header file to contain an extern declaration of the variable.
+// The header is included by the one source file that defines the variable 
+// and by all the source files that reference the variable. 
+// For each program, one source file (and only one source file) defines the variable. 
+// Similarly, one header file (and only one header file) should declare the variable. 
+// The header file is crucial; it enables cross-checking between
+// independent TUs (translation units — think source files) and ensures consistency.
+
+// Example: in our case global vars are declared as extern in display.h (this file),
+// then defined in display.c and can be referenced in main.c (and other .c files) by including display.h
+extern SDL_Window* window;
+extern SDL_Renderer* renderer;
+
+extern uint32_t* color_buffer; //pointer to first element in 1D array of 4 byte color values
+extern SDL_Texture* color_buffer_texture;
+
+extern int window_width; // int just for code simplicity according to pikuma
+extern int window_height; // could also be uint32_t etc
+
+bool initialize_window(void);
+void draw_grid(void);
+void draw_rect(int x, int y, int width, int height, uint32_t color);
+void render_color_buffer(void);
+void clear_color_buffer(uint32_t color);
+void destroy_window(void);
+
+#endif
