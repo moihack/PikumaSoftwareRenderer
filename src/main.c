f7a7a0936888e4854c6cc44ff382a98fb51cda6c
moihack <moihack@users.noreply.github.com>
2024-03-06 20:42:19 +0200

    Vector Rotation Function
    Lectures:
    Vector Transformations
    Review of Sine, Cosine and Tangent
    Rotating Vectors
    
    did not have any code changes.

--- src/main.c
+++ src/main.c
@@ -15,6 +15,7 @@ vec3_t cube_points[N_POINTS]; // 9x9x9 cube
 vec2_t projected_points[N_POINTS];
 
 vec3_t camera_position = { .x = 0, .y = 0, .z = -5 };
+vec3_t cube_rotation = { .x = 0, .y = 0, .z = 0 };
 
 float fov_factor = 640;
 
@@ -84,15 +85,23 @@ vec2_t project(vec3_t point)
 
 void update(void)
 {
+	cube_rotation.x += 0.01;
+	cube_rotation.y += 0.01; //increase y rotation each frame
+	cube_rotation.z += 0.01;
+
 	for (int i = 0; i < N_POINTS; i++)
 	{
 		vec3_t point = cube_points[i];
 
+		vec3_t transformed_point = vec3_rotate_x(point, cube_rotation.x);
+		transformed_point = vec3_rotate_y(transformed_point, cube_rotation.y);
+		transformed_point = vec3_rotate_z(transformed_point, cube_rotation.z);
+
 		//Move the points away from the camera
-		point.z -= camera_position.z;
+		transformed_point.z -= camera_position.z;
 
 		//project the current point
-		vec2_t projected_point = project(point);
+		vec2_t projected_point = project(transformed_point);
 
 		//save the projected 2D Vector in the array of projected points
 		projected_points[i] = projected_point;
@@ -108,8 +117,8 @@ void render(void)
 	{
 		vec2_t projected_point = projected_points[i];
 		draw_rect(
-			projected_point.x + (window_width/2),
-			projected_point.y + (window_height/2),
+			projected_point.x + (window_width / 2),
+			projected_point.y + (window_height / 2),
 			4,
 			4,
 			0xFFFFFF00 //yellow (ARGB8888)
