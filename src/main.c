66544b5ed07567a9aee2a72c314466fdf6b5e333
moihack <moihack@users.noreply.github.com>
2024-03-25 12:54:55 +0200

    Dynamic Arrays
    array.c and array.h taken from https://github.com/gustavopezzi/dynamicarray

--- src/main.c
+++ src/main.c
@@ -2,11 +2,13 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <SDL.h>
+#include "array.h"
 #include "display.h"
 #include "vector.h"
 #include "mesh.h"
 
-triangle_t triangles_to_render[N_MESH_FACES];
+// Array of triangles that should be rendered frame by frame
+triangle_t* triangles_to_render = NULL;
 
 vec3_t camera_position = { .x = 0, .y = 0, .z = -5 };
 vec3_t cube_rotation = { .x = 0, .y = 0, .z = 0 };
@@ -95,6 +97,9 @@ void update(void)
 	}
 	previous_frame_time = SDL_GetTicks();
 
+	// Initialize the array of triangles to render
+	triangles_to_render = NULL;
+
 	cube_rotation.x += 0.01;
 	cube_rotation.y += 0.01; //increase y rotation each frame
 	cube_rotation.z += 0.01;
@@ -138,7 +143,7 @@ void update(void)
 		}
 
 		// Save the projected triangle in the array of triangles to render
-		triangles_to_render[i] = projected_triangle;
+		array_push(triangles_to_render, projected_triangle);
 	}
 }
 
@@ -146,8 +151,9 @@ void render(void)
 {
 	draw_grid();
 
-	//Loop all projected triangles and render them	
-	for (int i = 0; i < N_MESH_FACES; i++)
+	//Loop all projected triangles and render them
+	int num_triangles = array_length(triangles_to_render);
+	for (int i = 0; i < num_triangles; i++)
 	{
 		triangle_t  triangle = triangles_to_render[i];
 
@@ -168,6 +174,9 @@ void render(void)
 			);
 	}
 	
+	// Clear the array of triangles to render every frame loop
+	array_free(triangles_to_render);
+
 	render_color_buffer();
 	clear_color_buffer(0xFF000000); // black (ARGB8888)
 
