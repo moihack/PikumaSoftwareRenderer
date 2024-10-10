881fbf9d111bec94faa1bb75a9797e9aa7795f18
moihack <moihack@users.noreply.github.com>
2024-10-10 17:07:35 +0300

    Avoiding Division by Zero
    Avoid dividing by zero (possible in fill_flat_<bottom|top>_triangle functions.
    In some edge cases a triangle will already be a flat top or flat bottom one meaning 2 of the vertices of the triangle will have the same y-coordinate leading to dividing by zero.
    By manually checking if a triangle is already flat-bottom or flat-top before getting filled we can avoid these edge cases.

--- src/triangle.c
+++ src/triangle.c
@@ -129,13 +129,29 @@ void draw_filled_triangle(int x0, int y0, int x1, int y1, int x2, int y2, uint32
 		int_swap(&x0, &x1);
 	}
 
-	// Calculate the new vertex (Mx, My) using triangle similarity
-	int My = y1;
-	int Mx = ((float)((x2 - x0) * (y1 - y0)) / (float) (y2 - y0)) + x0;
+	// avoid "division by zero" cases inside fill_flat functions
+	// these can occur when a triangle is already (after all transformations) a flat-top or flat-bottom one
 
-	// Draw flat-bottom triangle
-	fill_flat_bottom_triangle(x0, y0, x1, y1, Mx, My, color);
+	if (y1 == y2) // see figure above function signature to visualize the "division by zero" case in your head
+	{
+		// If true we simply have to draw a flat-bottom triangle
+		fill_flat_bottom_triangle(x0, y0, x1, y1, x2, y2, color);
+	}
+	else if (y0 == y1) // see figure above function signature to visualize the "division by zero" case in your head
+	{
+		// If true we simply have to draw a flat-top triangle
+		fill_flat_top_triangle(x0, y0, x1, y1, x2, y2, color);
+	}
+	else // generic case like shown in figure above function signature
+	{
+		// Calculate the new vertex (Mx, My) using triangle similarity
+		int My = y1;
+		int Mx = ((float)((x2 - x0) * (y1 - y0)) / (float)(y2 - y0)) + x0;
 
-	// Draw flat-top triangle
-	fill_flat_top_triangle(x1, y1, Mx, My, x2, y2, color);
+		// Draw flat-bottom triangle
+		fill_flat_bottom_triangle(x0, y0, x1, y1, Mx, My, color);
+
+		// Draw flat-top triangle
+		fill_flat_top_triangle(x1, y1, Mx, My, x2, y2, color);
+	}
 }
