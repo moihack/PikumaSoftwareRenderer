9f7fc9e5bf6a25477777cb6de955aa26245978bb
moihack <moihack@users.noreply.github.com>
2024-10-19 19:29:32 +0300

    Texture Typedef
    Texture Mapping & Representing Textures in Memory lessons did not feature any code changes. Only high level explanations on how texture mapping and uv coords work.
    
    Faces and Triangles were modified to also carry uv coords for each vertex/point
    texture.c adds a hardcoded texture value to be used with the hardcoded cube for the time being

--- src/display.h
+++ src/display.h
@@ -35,7 +35,9 @@ enum render_method
 	RENDER_WIRE,
 	RENDER_WIRE_VERTEX,
 	RENDER_FILL_TRIANGLE,
-	RENDER_FILL_TRIANGLE_WIRE
+	RENDER_FILL_TRIANGLE_WIRE,
+	RENDER_TEXTURED,
+	RENDER_TEXTURED_WIRE
 } extern render_method; // read extern comment below
 
 // only declaration
--- src/triangle.h
+++ src/triangle.h
@@ -2,21 +2,35 @@
 #define TRIANGLE_H
 
 #include <stdint.h>
+#include "texture.h"
 #include "vector.h"
 
 typedef struct {
 	int a;
 	int b;
 	int c;
+	tex2_t a_uv;
+	tex2_t b_uv;
+	tex2_t c_uv;
 	uint32_t color;
 } face_t; // stores vertex indices for each face/triangle of a mesh
 
 typedef struct {
 	vec2_t points[3];
+	tex2_t texcoords[3];
 	uint32_t color;
 	float avg_depth;
 } triangle_t; // stores the actual vec2 points of the triangle in the screen
 
+// Note: Pikuma has draw_triangle moved here in lesson "Texture Typedef".
+// However I do not recall moving the function here in any of the lessons.
+// For the time being I'll keep the draw_triangle function in display.h/.c
 void draw_filled_triangle(int x0, int y0, int x1, int y1, int x2, int y2, uint32_t color);
+void draw_textured_triangle(
+	int x0, int y0, float u0, float v0, // vertex A
+	int x1, int y1, float u1, float v1, // vertex B
+	int x2, int y2, float u2, float v2, // vertex C
+	uint32_t* texture
+);
 
 #endif
